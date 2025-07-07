4cbfdc1a146f4cbccffe2bc348e3c9a37d5b7dde
ImeshaDilshani <imeshadilshani212@gmail.com>
2025-07-07 21:54:56 +0530

    added mbart50

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 # app/main.py
 from fastapi import FastAPI, UploadFile, File
-from app.services import asr_service
+from app.services import asr_service, translation_service
 
-app = FastAPI(title="SpeechChain LK")
+app = FastAPI(title="SpeechChain LK API")
 
-@app.post("/asr/")
-async def asr_endpoint(file: UploadFile = File(...)):
+@app.post("/speechchain/")
+async def asr_and_translate(file: UploadFile = File(...)):
     audio_bytes = await file.read()
+    
+    # 1️⃣ Transcribe Tamil
     tamil_text = asr_service.transcribe(audio_bytes)
-    return {"transcription": tamil_text}
+    
+    # 2️⃣ Translate to Sinhala
+    sinhala_text = translation_service.translate(tamil_text)
+
+    return {
+        "tamil_transcription": tamil_text,
+        "sinhala_translation": sinhala_text
+    }
--- app/services/translation_service.py
+++ app/services/translation_service.py
@@ -1,14 +1,20 @@
-# # app/services/translation_service.py
-# from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
+# app/services/translation_service.py
+from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 
-# # Load mBART50 model (load only once)
-# tokenizer = MBart50TokenizerFast.from_pretrained("app/models/mbart50")
-# model = MBartForConditionalGeneration.from_pretrained("app/models/mbart50")
+# Load tokenizer and model (once)
+tokenizer = MBart50TokenizerFast.from_pretrained("app/models/mbart50")
+model = MBartForConditionalGeneration.from_pretrained("app/models/mbart50")
 
-# tokenizer.src_lang = "ta_IN"
+# Set source language (Tamil)
+tokenizer.src_lang = "ta_IN"
 
-# def translate(tamil_text):
-#     inputs = tokenizer(tamil_text, return_tensors="pt")
-#     generated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id["si_LK"])
-#     sinhala_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
-#     return sinhala_text
+def translate(tamil_text):
+    # Tokenize Tamil input
+    inputs = tokenizer(tamil_text, return_tensors="pt")
+
+    # Generate Sinhala translation
+    generated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id["si_LK"])
+
+    sinhala_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
+
+    return sinhala_text
